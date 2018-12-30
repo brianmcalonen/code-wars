4f9a52275aaa8156378ee3082f3c015f8c209931
Brian McAlonen <brianm0623@gmail.com>
2018-12-29 19:57:47 -0800

    added algorithm

--- app.js
+++ app.js
@@ -3,18 +3,39 @@
 //////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////
-// Fibonacci
+// Memoized Fibonacci
 
-function fibonacci(position) {
+function fibMemo(index, cache) {
+  // cache equals cache that was passed in or empty array
+  cache = cache || [];
 
-  if (position < 3) {
-    return 1
-  } else {
-    return (fibonacci(position - 1) + fibonacci(position - 2))
+  if (cache[index]) return cache[index];
+  else {
+    if (index < 3) return 1;
+    else {
+      cache[index] = fibMemo(index - 1, cache) + fibMemo(index - 2, cache)
+    }
   }
+
+  return cache[index]
 }
 
-console.log(fibonacci(20))
+console.log(fibMemo(15))
+
+//////////////////////////////////////////////////////////////////
+// Fibonacci
+
+// function fibonacci(position) {
+
+//   if (position < 3) {
+//     return 1
+//   } else {
+//     return (fibonacci(position - 1) + fibonacci(position - 2))
+//   }
+// }
+
+// console.log(fibonacci(20))
+
 //////////////////////////////////////////////////////////////////
 // Binary Search
 
